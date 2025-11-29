14beef0af3029616f4081f857e60733ab1948c16
priyanshuraj.debug <rajpriyanshu964@gmail.com>
2025-11-29 11:37:56 +0530

    feat: Implement createTweet and getUserTweets functions with validation and error handling

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -7,10 +7,38 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
     //TODO: create tweet
+    const {content}=req.body
+    if(!content||content.trim()===""){
+        throw new ApiError(400,"Content required")
+    }
+    const tweet= await Tweet.create({
+        content:content.trim(),
+        owner:req.user._id
+    })
+    return res.status(201).json(new ApiResponse(200,tweet,"tweet Created successfully"))
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
+    const {userId}=req.params
+    if(!userId||!isValidObjectId(userId)){
+        throw new ApiError(400,"Invalid user")
+    }
+    const user=await User.findById(userId)
+    if(!user){
+        throw new ApiError(404,"User not found")
+    }
+    const userTweet= await Tweet.aggregate([
+       {
+            $match: { owner: new mongoose.Types.ObjectId(userId) }
+        },
+        {
+            $sort: { createdAt: -1 } // latest first
+        }
+    ])
+    console.log(userTweet);
+    
+    return res.status(200).json(new ApiResponse(201,userTweet,"user tweet fetched successsfully"))
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
