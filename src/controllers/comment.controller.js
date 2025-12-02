aff076de4fd42ca6943822e58754be259a134c68
priyanshuraj.debug <rajpriyanshu964@gmail.com>
2025-12-02 18:48:35 +0530

    feat: Add validation and error handling for comment operations

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,8 +1,9 @@
-import mongoose from "mongoose"
+import mongoose, { isValidObjectId } from "mongoose"
 import {Comment} from "../models/comment.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+import {Video} from "../models/video.model.js"
 
 const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
@@ -13,14 +14,69 @@ const getVideoComments = asyncHandler(async (req, res) => {
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
+    const {content}=req.body
+    const {videoId}=req.params
+        if(!content||content.trim()===""){
+        throw new ApiError(400,"Content required")
+    }
+        if(!videoId||!isValidObjectId(videoId)){
+        throw new ApiError(400,"invalid id")
+    }
+       
+    const video=await Video.findById(videoId)
+    if(!video){
+        throw new ApiError(400,"Video not found")
+    }
+    
+      const comment = await Comment.create({
+        content,
+        video: video._id,
+        owner: req.user._id
+    });
+    
+    return res.status(200).json(new ApiResponse(200,comment,"Comment created successfully"))
 })
 
 const updateComment = asyncHandler(async (req, res) => {
     // TODO: update a comment
+    const {commentId}=req.params
+    const {newContent}=req.body
+    if(!commentId||!isValidObjectId(commentId)){
+        throw new ApiError(400,"invalid CommentId")
+    }
+    if(!newContent||newContent.trim()===""){
+        throw new ApiError(400,"new content required")
+    }
+    const comment=await Comment.findById(commentId)
+    if(!comment){
+        throw new ApiError(400,"Video not found")
+    }
+    if(comment.owner.toString()!==req.user._id.toString()){
+        throw new ApiError(402,"You are not authorized to update comment")
+    }
+    const updatedComment=await Comment.findByIdAndUpdate(commentId,{
+        $set:{
+            content:newContent
+        }
+    },{new:true})
+    return res.status(200).json(new ApiResponse(200,updatedComment,"Comment updated successfully"))
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
+    const {commentId}=req.params
+    if(!commentId||!isValidObjectId(commentId)){
+        throw new ApiError(400,"invalid commentId")
+    }
+    const comment=await Comment.findById(commentId)
+    if(!comment){
+        throw new ApiError(400,"Comment not found")
+    }
+    if(comment.owner.toString()!==req.user._id.toString()){
+        throw new ApiError(402,"You are not authorized to delete this tweet")
+    }
+    await Comment.findByIdAndDelete(commentId)
+    return res.status(200).json(new ApiResponse(200,{},"Comment updated successfully"))
 })
 
 export {
