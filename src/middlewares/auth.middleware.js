a26858d02149e9bda1aea73c4a9198b3c9015fdd
priyanshuraj.debug <rajpriyanshu964@gmail.com>
2026-01-11 11:04:54 +0530

    fix: add logging for request and token verification in JWT middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,13 +2,15 @@ import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken"
 import { User } from "../models/user.model.js";
+import { response } from "express";
 
 export const verifyJWT = asyncHandler(async(req, _, next) => {
+    console.log(req)
     try {
          const token =req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
 
         
-        // console.log(token);
+         console.log(token);
         if (!token) {
             throw new ApiError(401, "Unauthorized request")
         }
@@ -25,6 +27,8 @@ export const verifyJWT = asyncHandler(async(req, _, next) => {
         req.user = user;
         next()
     } catch (error) {
+        console.log("from verify jwt error",error);
+        
         throw new ApiError(401, error?.message || "Invalid access token")
     }
     
