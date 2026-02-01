3a1e3a0b55cd430397c8befaf73e5fe84fa1c42e
priyanshuraj.debug <rajpriyanshu964@gmail.com>
2026-02-01 14:28:12 +0530

    fix: apply verifyJWT middleware to all video routes for enhanced security

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -11,12 +11,12 @@ import {verifyJWT} from "../middlewares/auth.middleware.js"
 import {upload} from "../middlewares/multer.middleware.js"
 
 const router = Router();
-router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+ // Apply verifyJWT middleware to all routes in this file
 
 router
     .route("/")
     .get(getAllVideos)
-    .post(
+    .post(verifyJWT,
         upload.fields([
             {
                 name: "videoFile",
@@ -33,10 +33,10 @@ router
 
 router
     .route("/:videoId")
-    .get(getVideoById)
-    .delete(deleteVideo)
-    .patch(upload.single("thumbnail"), updateVideo);
+    .get(verifyJWT,getVideoById)
+    .delete(verifyJWT,deleteVideo)
+    .patch(verifyJWT,upload.single("thumbnail"), updateVideo);
 
-router.route("/toggle/publish/:videoId").patch(togglePublishStatus);
+router.route("/toggle/publish/:videoId").patch(verifyJWT,togglePublishStatus);
 
 export default router
